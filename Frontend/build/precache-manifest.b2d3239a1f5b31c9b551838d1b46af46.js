da75260b300d6b69eedc48b6d4d5dd8f3bdb0439
Sebastian Kulle <sebastian.kulle@gmail.com>
2022-06-23 21:30:24 +0200

    build

---
+++ Frontend/build/precache-manifest.b2d3239a1f5b31c9b551838d1b46af46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83ca611ab337947d9cdb",
+    "url": "/static/css/main.19689d53.chunk.css"
+  },
+  {
+    "revision": "83ca611ab337947d9cdb",
+    "url": "/static/js/main.83ca611a.chunk.js"
+  },
+  {
+    "revision": "280751ef266f00169e2d",
+    "url": "/static/js/1.280751ef.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e9f48487b6387bc719280b9561f537d",
+    "url": "/index.html"
+  }
+];
